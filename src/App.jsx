15e4b9916807a5dda7b4d94ed4a37777822b1d7b
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-02-09 18:21:53 +0600

    [DONE] Navbar added with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main className="min-w-[260px] md:max-w-xl mx-auto min-h-screen flex flex-col items-center gap-8 mt-8 px-10 sm:px-12 md:px-14">
+    <main className="main-container flex flex-col items-center gap-8">
       <h2 className="text-2xl font-semibold">Qtech Todo</h2>
       <Navbar />
     </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,28 @@ const Navbar = () => {
   const [searchParams] = useSearchParams();
   const todosParams = searchParams.get("todos");
   return (
-    <nav className="flex flex-row items-center justify-between min-w-[320px] md:max-w-xl border-b border-[#ccc]">
+    <nav className="flex flex-row items-center justify-between min-w-[300px] sm:min-w-[450px] md:min-w-full border-b border-[#ccc]">
       <Link
         to="/"
-        className={`${todosParams === null ? "active" : "non-active"}`}
+        className={`${
+          todosParams === null ? "active" : "non-active"
+        } sm:text-lg md:text-xl xl:text-2xl`}
       >
         All
       </Link>
       <Link
         to="/?todos=active"
-        className={`${todosParams === "active" ? "active" : "non-active"}`}
+        className={`${
+          todosParams === "active" ? "active" : "non-active"
+        } sm:text-lg md:text-xl xl:text-2xl`}
       >
         Active
       </Link>
       <Link
         to="/?todos=completed"
-        className={`${todosParams === "completed" ? "active" : "non-active"}`}
+        className={`${
+          todosParams === "completed" ? "active" : "non-active"
+        } sm:text-lg md:text-xl xl:text-2xl`}
       >
         Completed
       </Link>
